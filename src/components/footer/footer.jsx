ff76a451beb7b83b0bdf0dc92aa53262a641974a
Rudra Prasad Sahu <srudra3110@gmail.com>
2022-10-22 14:03:42 +0530

    my first commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <h1 className="footer__title">Rudra</h1>
+
+                <ul className="footer__list">
+                    <li>
+                        <a href="#about" className="footer__link">About</a>
+                    </li>
+                    <li>
+                        <a href="#projects" className="footer__link">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#about" className="footer__link">Testimonials</a>
+                    </li>
+                </ul>
+
+                <div className="footer__social">
+                    <a href="" className="footer__social-link" target="_blank">
+                        <i class="bx bxl-instagram"></i>
+                    </a>
+                    <a href="" className="footer__social-link" target="_blank">
+                        <i class="bx bxl-instagram"></i>
+                    </a>
+                    <a href="" className="footer__social-link" target="_blank">
+                        <i class="bx bxl-github"></i>
+                    </a>
+                </div>
+
+                <span className="footer__copy">
+                    &#169; Rudra Prasad Sahu. All rights reserved
+                </span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
